11bb99bac56e51cf35e4f3988a9a71c9be7830a0
rlawjggns <rlawjggns12@gmail.com>
2024-05-02 15:55:41 +0900

    king

---
+++ wh.py
@@ -0,0 +1,6 @@
+partyA = set(["Park", "Kim", "Lee"])
+partyB = set(["Park", "Choi"])
+print("파티 A=", partyA)
+print("파티 B=", partyB)
+print (f"동시에 참석한 사람={partyA^partyB}")
+print("으아아아ㅏ아아아아ㅏ아아아아아아아")
